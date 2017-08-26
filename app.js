d4d5b93467dac695f783a0f737e0f10d0da71d68
mannycov <manny.cov09@gmail.com>
2017-08-25 21:14:16 -0700

    added feature that toggles crossed-out style when a list item is clicked

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+var App = () => (
+  <div>
+    <h1>My GroceryList</h1>
+    <GroceryList items={['Cucumbers', 'Kale', 'Cheetos']}/>
+  </div>
+);
+
 var GroceryList = (props) => (
   <ul>
-    <h1>My GroceryList</h1>
     {props.items.map(item =>
       <GroceryListItem item={item} />
       )}
@@ -11,15 +17,32 @@ class GroceryListItem extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  // When a list item is clicked, toggle the `done` boolean and the component's `render` method will run again
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
   }
 
   render() {
 
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
     return (
-      <li>{this.props.item}</li>
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
     );
   }
 
 }
 
-ReactDOM.render(<GroceryList />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
+
