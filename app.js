492927a0e1375e7d344f8fbc99f12e0c62b2124a
mannycov <manny.cov09@gmail.com>
2017-08-25 20:26:04 -0700

    created and rendered React components and nested components

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// TODO
+var GroceryList = () => (
+  <ul>
+    <h1>My GroceryList</h1>
+    <Cucumbers />
+    <Kale />
+  </ul>
+);
+
+var Cucumbers = () => (
+  <li>Cucumbers</li>
+);
+
+var Kale = () => (
+  <li>Kale</li>
+);
+
+// var GroceryListItem = (props) => (
+
+// );
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
